f01e837cc7aa2afe0a4c5cb9f1c428881b383e9e
hryou0922 <hryou0922@126.com>
2017-02-26 14:20:38 +0800

    log

--- log/src/main/java/com/hry/spring/log/LogApplication.java
+++ log/src/main/java/com/hry/spring/log/LogApplication.java
@@ -9,7 +9,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LogApplication {
 	private static final Logger log = LoggerFactory.getLogger(LogApplication.class);
 	public static void main(String[] args) {
-		log.info("Begin Start ...");
+		String str1 = "string1";
+		String str2 = "string2";
+		log.info("Begin Start {}...{}", str1, str2);
+	//	log.info("Begin Start " + str1 +"..." + str2);
 		SpringApplication.run(LogApplication.class, args);
 		log.info("Stop ...");
 	}
