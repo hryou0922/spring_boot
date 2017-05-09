0f5b5c5d0f0393bcfdee422941fe0f0bc35cf447
hryou0922 <hryou0922@126.com>
2017-05-09 19:49:04 +0800

    offical stream demo

---
+++ grpc/readme.md
@@ -0,0 +1,7 @@
+com.hry.spring.grpc.simple
+	hello world
+com.hry.spring.grpc.stream
+	官方例子
+
+com.hry.spring.grpc.util
+	工具类
---
+++ grpc/src/main/java/com/hry/spring/grpc/stream/custom/RouteGuideServer.java
@@ -0,0 +1,256 @@
+package com.hry.spring.grpc.stream.custom;
+
+import static java.lang.Math.atan2;
+import static java.lang.Math.cos;
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+import static java.lang.Math.sin;
+import static java.lang.Math.sqrt;
+import static java.lang.Math.toRadians;
+import static java.util.concurrent.TimeUnit.NANOSECONDS;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.hry.spring.grpc.stream.Feature;
+import com.hry.spring.grpc.stream.Point;
+import com.hry.spring.grpc.stream.Rectangle;
+import com.hry.spring.grpc.stream.RouteGuideGrpc;
+import com.hry.spring.grpc.stream.RouteNote;
+import com.hry.spring.grpc.stream.RouteSummary;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.stub.StreamObserver;
+
+
+
+public class RouteGuideServer {
+  private static final Logger logger = Logger.getLogger(RouteGuideServer.class.getName());
+
+  private final int port;
+  private final Server server;
+
+  public RouteGuideServer(int port) throws IOException {
+    this(port, RouteGuideUtil.getDefaultFeaturesFile());
+  }
+
+  public RouteGuideServer(int port, URL featureFile) throws IOException {
+    this(ServerBuilder.forPort(port), port, RouteGuideUtil.parseFeatures(featureFile));
+  }
+
+  public RouteGuideServer(ServerBuilder<?> serverBuilder, int port, Collection<Feature> features) {
+    this.port = port;
+    server = serverBuilder.addService(new RouteGuideService(features))
+        .build();
+  }
+
+  // 启动服务
+  public void start() throws IOException {
+    server.start();
+    logger.info("Server started, listening on " + port);
+    Runtime.getRuntime().addShutdownHook(new Thread() {
+      @Override
+      public void run() {
+        System.err.println("*** shutting down gRPC server since JVM is shutting down");
+        RouteGuideServer.this.stop();
+        System.err.println("*** server shut down");
+      }
+    });
+  }
+
+  // 启动服务
+  public void stop() {
+    if (server != null) {
+      server.shutdown();
+    }
+  }
+
+  /**
+   * Await termination on the main thread since the grpc library uses daemon threads.
+   */
+  private void blockUntilShutdown() throws InterruptedException {
+    if (server != null) {
+      server.awaitTermination();
+    }
+  }
+  
+  public static void main(String[] args) throws Exception {
+    RouteGuideServer server = new RouteGuideServer(8980);
+    server.start();
+    server.blockUntilShutdown();
+  }
+
+  /**
+   * 服务端类的实现，继承RouteGuideGrpc.RouteGuideImplBase
+   *
+   */
+  private static class RouteGuideService extends RouteGuideGrpc.RouteGuideImplBase {
+    private final Collection<Feature> features;
+    private final ConcurrentMap<Point, List<RouteNote>> routeNotes =
+        new ConcurrentHashMap<Point, List<RouteNote>>();
+
+    RouteGuideService(Collection<Feature> features) {
+      this.features = features;
+    }
+    
+    @Override
+    public void getFeature(Point request, StreamObserver<Feature> responseObserver) {
+      responseObserver.onNext(checkFeature(request));
+      responseObserver.onCompleted();
+    }
+
+    /**
+     * 根据Rectangle获取feature里的值
+     */
+    @Override
+    public void listFeatures(Rectangle request, StreamObserver<Feature> responseObserver) {
+      int left = min(request.getLo().getLongitude(), request.getHi().getLongitude());
+      int right = max(request.getLo().getLongitude(), request.getHi().getLongitude());
+      int top = max(request.getLo().getLatitude(), request.getHi().getLatitude());
+      int bottom = min(request.getLo().getLatitude(), request.getHi().getLatitude());
+
+      for (Feature feature : features) {
+        if (!RouteGuideUtil.exists(feature)) {
+          continue;
+        }
+
+        int lat = feature.getLocation().getLatitude();
+        int lon = feature.getLocation().getLongitude();
+        if (lon >= left && lon <= right && lat >= bottom && lat <= top) {
+          responseObserver.onNext(feature);
+        }
+      }
+      responseObserver.onCompleted();
+    }
+
+    /**
+     * 接收多个Point，生成一个总的返回值RouteSummary
+     */
+    @Override
+    public StreamObserver<Point> recordRoute(final StreamObserver<RouteSummary> responseObserver) {
+      return new StreamObserver<Point>() {
+        int pointCount;
+        int featureCount;
+        int distance;
+        Point previous;
+        final long startTime = System.nanoTime();
+
+        @Override
+        public void onNext(Point point) {
+          pointCount++;
+          if (RouteGuideUtil.exists(checkFeature(point))) {
+            featureCount++;
+          }
+          // For each point after the first, add the incremental distance from the previous point to
+          // the total distance value.
+          if (previous != null) {
+            distance += calcDistance(previous, point);
+          }
+          previous = point;
+        }
+
+        @Override
+        public void onError(Throwable t) {
+          logger.log(Level.WARNING, "recordRoute cancelled");
+        }
+
+        @Override
+        public void onCompleted() {
+          long seconds = NANOSECONDS.toSeconds(System.nanoTime() - startTime);
+          responseObserver.onNext(RouteSummary.newBuilder().setPointCount(pointCount)
+              .setFeatureCount(featureCount).setDistance(distance)
+              .setElapsedTime((int) seconds).build());
+          responseObserver.onCompleted();
+        }
+      };
+    }
+
+    /**
+     * Receives a stream of message/location pairs, and responds with a stream of all previous
+     * messages at each of those locations.
+     *
+     * @param responseObserver an observer to receive the stream of previous messages.
+     * @return an observer to handle requested message/location pairs.
+     */
+    @Override
+    public StreamObserver<RouteNote> routeChat(final StreamObserver<RouteNote> responseObserver) {
+      return new StreamObserver<RouteNote>() {
+        @Override
+        public void onNext(RouteNote note) {
+          List<RouteNote> notes = getOrCreateNotes(note.getLocation());
+
+          // Respond with all previous notes at this location.
+          for (RouteNote prevNote : notes.toArray(new RouteNote[0])) {
+        	// 返回客户端的内容
+            responseObserver.onNext(prevNote);
+          }
+
+          // Now add the new note to the list
+          notes.add(note);
+        }
+
+        @Override
+        public void onError(Throwable t) {
+          logger.log(Level.WARNING, "routeChat cancelled");
+        }
+
+        @Override
+        public void onCompleted() {
+          responseObserver.onCompleted();
+        }
+      };
+    }
+
+    /**
+     * Get the notes list for the given location. If missing, create it.
+     */
+    private List<RouteNote> getOrCreateNotes(Point location) {
+      List<RouteNote> notes = Collections.synchronizedList(new ArrayList<RouteNote>());
+      List<RouteNote> prevNotes = routeNotes.putIfAbsent(location, notes);
+      return prevNotes != null ? prevNotes : notes;
+    }
+
+    // 根据Point的内容从featrue列表中获取对应feature
+    private Feature checkFeature(Point location) {
+      for (Feature feature : features) {
+        if (feature.getLocation().getLatitude() == location.getLatitude()
+            && feature.getLocation().getLongitude() == location.getLongitude()) {
+          return feature;
+        }
+      }
+
+      // 没有找到feature，生成一个默认值
+      return Feature.newBuilder().setName("").setLocation(location).build();
+    }
+
+    /**
+     * 计算节点的距离
+     */
+    private static int calcDistance(Point start, Point end) {
+      double lat1 = RouteGuideUtil.getLatitude(start);
+      double lat2 = RouteGuideUtil.getLatitude(end);
+      double lon1 = RouteGuideUtil.getLongitude(start);
+      double lon2 = RouteGuideUtil.getLongitude(end);
+      int r = 6371000; // meters
+      double phi1 = toRadians(lat1);
+      double phi2 = toRadians(lat2);
+      double deltaPhi = toRadians(lat2 - lat1);
+      double deltaLambda = toRadians(lon2 - lon1);
+
+      double a = sin(deltaPhi / 2) * sin(deltaPhi / 2)
+          + cos(phi1) * cos(phi2) * sin(deltaLambda / 2) * sin(deltaLambda / 2);
+      double c = 2 * atan2(sqrt(a), sqrt(1 - a));
+
+      return (int) (r * c);
+    }
+  }
+}
