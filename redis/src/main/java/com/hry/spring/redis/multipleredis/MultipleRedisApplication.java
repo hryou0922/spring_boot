10ade356e5ef5ac22ddebe1d87389dbf4bb6cb89
hryou0922 <hryou0922@126.com>
2018-03-30 10:43:09 +0800

    update

---
+++ redis/src/main/java/com/hry/spring/redis/multipleredis/MultipleRedisApplication.java
@@ -0,0 +1,12 @@
+package com.hry.spring.redis.multipleredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipleRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultipleRedisApplication.class, args);
+	}
+}
