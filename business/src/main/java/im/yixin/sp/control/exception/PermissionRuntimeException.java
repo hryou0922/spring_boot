63bf03f572c39bc721273c87a7a24ad4458509ef
hryou0922 <hryou0922@126.com>
2019-01-23 17:44:18 +0800

    1. spring websocket通过rabbbit+redis实现集群

---
+++ business/src/main/java/im/yixin/sp/control/exception/PermissionRuntimeException.java
@@ -0,0 +1,9 @@
+package im.yixin.sp.control.exception;
+
+/**
+ * 异常类
+ * @Author: huangrongyou@yixin.im
+ * @Date: 2019/1/23 17:00:21
+ */
+public class PermissionRuntimeException extends RuntimeException {
+}
